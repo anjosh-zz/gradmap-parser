d693bbcf889383adade6a820fb58de71d1f46cc4
anjosh <joshuaan1@gmail.com>
2014-10-16 11:18:04 -0400

    *updates notes with updates for Course data structure *fix OR group bug *added special course constant for second writing course

--- parser/parser.py
+++ parser/parser.py
@@ -1,10 +1,26 @@
 import re
 
 # Parser for Course Bulletin page (https://web.cse.ohio-state.edu/cgi-bin/portal/report_manager/list.pl?r=12)
-#TODO might want to try parsing from CSE Semester Courses page (http://coe-portal.cse.ohio-state.edu/pdf-exports/CSE/) instead
+# TODO might want to try parsing from CSE Semester Courses page (http://coe-portal.cse.ohio-state.edu/pdf-exports/CSE/) instead
+
+# Special Class definitions
+SECOND_WRIT = ('ENGR', 2367)
+
+# TODO might want to use course objects instead
+# of tuples
+# TODO maybe even just put the prereqs in a class
+# attribute instead of having a dictionary of 
+# courses mapped to prereqs
+class Course():
+    def __init__(dept, number):
+        self.dept = dept
+        self.number = number
 
 def parse(prereq_data):
-    m_obj = re.search(r"Prereq: (.*).", prereq_data);
+    # TODO make courses dictionary matching course
+    # to it's prereqs
+    courses_dict = {}
+    m_obj = re.search(r"Prereq: (.*?)\.", prereq_data);
     prereq_str = m_obj.group(1)
     print prereq_str
 
@@ -20,7 +36,11 @@ def parse(prereq_data):
         if 'or' in and_group:
             print 'OR group found'
             or_list = add_courses(and_group, or_list)
-            result.append(or_list)
+            if len(or_list) == 1:
+                result.append(or_list[0])
+            else:
+                result.append(or_list)
+            or_list = []
         else:
             result = add_courses(and_group, result)
 
@@ -30,18 +50,30 @@ def parse(prereq_data):
 def add_courses(courses, to_list):
     c_list = courses.split(',')
     for c in c_list:
-        #TODO add match for course deptarment
-        m = re.search(r"(\w)?\d{4}", c)
-        if m:
-            c = int(m.group())
-            to_list.append(c)
+        if c == "Gen Ed Writing Level 2":
+            to_list.append(SECOND_WRIT)
+        else:
+            # might need to look for course only at
+            # beginning of group of courses 
+            m = re.search(r"(\w{3,4})? ?(\d{4})", c)
+            if m:
+                if m.group(1):
+                    dept = m.group(1)
+                else:
+                    dept = 'CSE'
+                c = int(m.group(2))
+                to_list.append(Course(dept, c))
     return to_list
 
 if __name__=='__main__':
-    tests = [ ("Prereq: 2231, 2331 (680), and 2421, and 3901 (560), 3902, or 3903.", [('CSE', 2231), ('CSE', 2331), ('CSE', 2421), [('CSE', 3901), ('CSE', 3902), ('CSE', 3903)]] ),
-            ("Description: Formal languages and grammars; recursive descent parsing; data types, expressions, control structures, parameter passing; compilers and interpreters; memory management; functional programming principles. Prereq: 2231, 2331 (680), and 2421, and 3901 (560), 3902, or 3903. Not open to students with credit for 5341 (655).", [('CSE', 2231), ('CSE', 2331), ('CSE', 2421), [('CSE', 3901), ('CSE', 3902), ('CSE', 3903)]] ),
-            ("2501 Social, Ethical, and Professional Issues in Computing U 1
-Description: Social, ethical, and professional issues facing computing professionals; ethical principles; discussion of case studies. Prereq: 1222, 1223, 2231, 214, 222, or 230, and 2321 or Math 366, and Gen Ed Writing Level 2. Not open to students with credit for 5501 (601).", [[('CSE', 1222), ('CSE', 1223), ('CSE', 2231)], ('CSE', 2321), ('ENGR', 2367), ]
+    f = open('data')
+    prereq_data = f.read()
+    tests = [
+            ("Prereq: 2231, 2331 (680), and 2421, and 3901 (560), 3902, or 3903.", {('CSE', 3341): [('CSE', 2231), ('CSE', 2331), ('CSE', 2421), [('CSE', 3901), ('CSE', 3902), ('CSE', 3903)]]}),
+            ("Description: Formal languages and grammars; recursive descent parsing; data types, expressions, control structures, parameter passing; compilers and interpreters; memory management; functional programming principles. Prereq: 2231, 2331 (680), and 2421, and 3901 (560), 3902, or 3903. Not open to students with credit for 5341 (655).", [('CSE', 2231), ('CSE', 2331), ('CSE', 2421), [('CSE', 3901), ('CSE', 3902), ('CSE', 3903)]]),
+            ("2501 Social, Ethical, and Professional Issues in Computing U 1\nDescription: Social, ethical, and professional issues facing computing professionals; ethical principles; discussion of case studies. Prereq: 1222, 1223, 2231, 214, 222, or 230, and 2321 or Math 366, and Gen Ed Writing Level 2. Not open to students with credit for 5501 (601).", [[('CSE', 1222), ('CSE', 1223), ('CSE', 2231)], ('CSE', 2321), ('ENGR', 2367)]),
+            (prereq_data, [('CSE', 2231), ('CSE', 2331), ('CSE', 2421), [('CSE', 3901), ('CSE', 3902), ('CSE', 3903)]]),
+            ]
 
     for test, answer in tests:
         result = parse(test)
